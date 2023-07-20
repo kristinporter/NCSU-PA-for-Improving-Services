93070c70290bb6ba60acd0b687114b3294acdafa
Kristin Porter <kristinporter@Kristins-MacBook-Pro.local>
2023-07-20 09:01:04 -0700

    adding files from earlier repo

---
+++ RenderBook.R
@@ -0,0 +1,12 @@
+library(bookdown)
+render_book(
+  input = ".",
+  output_format = NULL,
+  clean = TRUE,
+  envir = parent.frame(),
+  clean_envir = !interactive(),
+  output_dir = NULL,
+  new_session = NA,
+  preview = FALSE,
+  config_file = "_quarto.yml"
+)
